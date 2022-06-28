2a3fe8847335f7ff43bfa444885b202500cd1fba
matiascapurro <matias.capurro@agilesolutions.com>
2022-06-27 22:02:19 -0300

    clase 4

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-alert("bienvenido a la calculadora de numeros primos, usted elije un numero y el programa le dice si es primo o no")
-var continuar = "si";
-var num;
-do{
-    num = prompt("Ingrese un numero");
-    let primo = true;
-    for (let i = 2; i < num; i++){
-        if (num%i == 0){
-            primo = false;
-            continue;
+
+const presupuesto = (primero) => {
+    let persona = prompt('Es una persona fisica? Responsa SI, o NO en caso de ser una empresa');
+    let nombre = prompt('Ingrese nombre');
+    let empresaDePersona = 'Personal';
+    let aver = 'Personal';
+    if (persona.toUpperCase() == 'SI'){
+        let empresaDePersona = prompt('habla en representacion de alguna empresa?');
+        if (empresaDePersona.toUpperCase() == 'SI'){
+            alert('a continuacion le pedimos que ingrese la informacion de la empresa');
+            empresaDePersona = presupuesto(false);
+            aver = empresaDePersona
         }
+    }else aver = nombre;
+    if (primero) {
+        let finde = prompt('es su evento en un dia del fin de semana?');
+        let personas = prompt('cuanta gente asistira al evento? (responder con numeros)');
+        let horas = prompt('cuantas horas durara el evento?');
+        let presupuesto = 0;
+        presupuesto += personas*horas*150
+        if (finde.toUpperCase() == 'SI'){
+            presupuesto*=1.25
+        }
+        alert(`Presupuesto para ${nombre} de ${personas} personas y ${horas} horas. Presupuesto total: $${presupuesto}
+         Motivo: Evento ${aver}`);
     }
-    alert(primo);
-    continuar = prompt("Desea calcular otr numero? Responda 'si' para continuar");
-    }while (continuar == "si");
+    else{
+        return nombre;
+    }
+}
+presupuesto(true);
